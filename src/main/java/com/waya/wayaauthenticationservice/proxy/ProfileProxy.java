17d1c76aea3ee26a9c07b7d6cb8a48ef1335bd59
chisom amalu <camalu@cellulant.com>
2021-01-20 13:33:36 +0100

    after removing duplicate dependency for model mapper

--- src/main/java/com/waya/wayaauthenticationservice/proxy/ProfileProxy.java
+++ src/main/java/com/waya/wayaauthenticationservice/proxy/ProfileProxy.java
@@ -2,6 +2,6 @@ package com.waya.wayaauthenticationservice.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "", url = "")
+//@FeignClient(name = "", url = "http://46.101.41.187:8080/")
 public interface ProfileProxy {
 }
