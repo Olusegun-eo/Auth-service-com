290216fedb5534b14805200ab25b62b8ed0af311
stantoju <toju2031@gmail.com>
2021-03-24 15:44:23 +0100

    Rectified endpoints

--- src/main/java/com/waya/wayaauthenticationservice/service/AdminService.java
+++ src/main/java/com/waya/wayaauthenticationservice/service/AdminService.java
@@ -1,12 +1,13 @@
 package com.waya.wayaauthenticationservice.service;
 
 import com.waya.wayaauthenticationservice.entity.Users;
+import com.waya.wayaauthenticationservice.pojo.UserPojo;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface AdminService {
-
     ResponseEntity getCorporateUsers();
-    ResponseEntity getUsersByRole(int roleId);
+    ResponseEntity getUsersByRole(long roleId);
+    ResponseEntity createUser(UserPojo userPojo, long roleId);
 }
