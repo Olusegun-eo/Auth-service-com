aaa012eef5a9d8ef9260e1b16ef87a8644f31752
citymanjoe <einjoku@gmail.com>
2021-07-05 11:26:23 +0100

    Role and Privilege

--- src/main/java/com/waya/wayaauthenticationservice/entity/Privilege.java
+++ src/main/java/com/waya/wayaauthenticationservice/entity/Privilege.java
@@ -1,14 +1,21 @@
 package com.waya.wayaauthenticationservice.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreType;
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Collection;
 
 @Entity
 @Setter
--- src/main/java/com/waya/wayaauthenticationservice/entity/Roles.java
+++ src/main/java/com/waya/wayaauthenticationservice/entity/Roles.java
@@ -12,7 +12,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
