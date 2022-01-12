4e92c4d5ac90d2733c0c004de12e2fc51e9b48c5
oluwasinaosenidynamo <83323277+oluwasinaosenidynamo@users.noreply.github.com>
2022-01-12 19:09:40 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 yes | cp -rf staging.yaml deploy.yaml
 
-sed -i 's/imgtag/$VERSION/' deploy.yaml
+sed -i "s/imgtag/$VERSION/" deploy.yaml
 
 aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $CLUSTER_NAME
 
