3f0270b20dba0781f7eb1a24fd5c8839136e4313
oluwasinaosenidynamo <83323277+oluwasinaosenidynamo@users.noreply.github.com>
2022-02-17 12:37:04 +0100

    Update NotificationResponsePojo.java

--- src/main/java/com/waya/wayaauthenticationservice/pojo/notification/NotificationResponsePojo.java
+++ src/main/java/com/waya/wayaauthenticationservice/pojo/notification/NotificationResponsePojo.java
@@ -3,6 +3,7 @@ package com.waya.wayaauthenticationservice.pojo.notification;
 import com.waya.wayaauthenticationservice.streams.StreamDataEmail;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
