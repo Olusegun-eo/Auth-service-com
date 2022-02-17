199ddb7618770c34a2c4eedb83e7414877f9486d
citymanjoe <einjoku@gmail.com>
2022-02-17 12:30:09 +0100

    WAYABANK-AUTH-EMAIL

--- src/main/java/com/waya/wayaauthenticationservice/pojo/notification/NotificationResponsePojo.java
+++ src/main/java/com/waya/wayaauthenticationservice/pojo/notification/NotificationResponsePojo.java
@@ -3,13 +3,16 @@ package com.waya.wayaauthenticationservice.pojo.notification;
 import com.waya.wayaauthenticationservice.streams.StreamDataEmail;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class NotificationResponsePojo {
     private String eventType;
     private String initiator;
     private String eventCategory;
     private StreamDataEmail data;
+    private String productType = "WAYABANK";
 
 }
