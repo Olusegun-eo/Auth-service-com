e60e01c8c31ba4f9bf068c1ed40f149de27eff3b
oluwasinaosenidynamo <83323277+oluwasinaosenidynamo@users.noreply.github.com>
2022-02-17 12:33:49 +0100

    Update MessagingService.java

--- src/main/java/com/waya/wayaauthenticationservice/service/impl/MessagingService.java
+++ src/main/java/com/waya/wayaauthenticationservice/service/impl/MessagingService.java
@@ -58,7 +58,7 @@ public class MessagingService {
             notificationResponsePojo.setProductType("WAYABANK");
 
             //      messageQueueProducer.send(EMAIL_TOPIC, post);
-            log.error("data {}", notificationResponsePojo.toString());
+            log.info("data {}", notificationResponsePojo.toString());
            proxy.sendEmail(notificationResponsePojo);
         // send to notification service
         } catch (Exception exception) {
