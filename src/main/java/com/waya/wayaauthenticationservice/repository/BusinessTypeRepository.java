504baaf5a0815d74eb83136deaa2a8774916b79d
agbeterseer <agbe.terseer@gmail.com>
2022-03-25 22:47:07 +0100

    update on Business Type

--- src/main/java/com/waya/wayaauthenticationservice/repository/BusinessTypeRepository.java
+++ src/main/java/com/waya/wayaauthenticationservice/repository/BusinessTypeRepository.java
@@ -1,5 +1,7 @@
 package com.waya.wayaauthenticationservice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import com.waya.wayaauthenticationservice.entity.BusinessType;
 public interface BusinessTypeRepository extends JpaRepository<BusinessType, Long> {
 
     boolean existsByBusinessTypeIgnoreCase(String businessType);
+
+    @Override
+    Page<BusinessType> findAll(Pageable pageable);
 }
